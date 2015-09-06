a02fcf2de81387fb2cf63f310127abca5d94ff73
chayan-DUET <chayan_cse10@yahoo.com>
2015-09-06 17:44:50 +0600

    statment
    just i had statment

--- GitJva/src/gitjva/GitJva.java
+++ GitJva/src/gitjva/GitJva.java
@@ -15,5 +15,6 @@ public class GitJva {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("we are larning github");
     }
 }
