8e2cdfb73ffe0b57079ceae64430f2ecfa80c0d0
chayan-DUET <chayan_cse10@yahoo.com>
2015-09-06 19:57:40 +0600

    View class
    just create view class

---
+++ GitJva/src/gitjva/ViewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitjva;
+
+/**
+ *
+ * @author Chayan_pc
+ */
+public class ViewClass {
+    
+}
